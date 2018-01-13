623850fdecb7f7ac241dee149b4de4c3239f15d3
Andrew Barrett-Sprot <abarrettsprot@gmail.com>
2018-01-13 18:01:13 +0000

    merge imaginary PR by @neosilky
    - allow `gen_titles.sh` to be run from anywhere
    - don't generate PDF files if they're already there

--- gen_titles.sh
+++ gen_titles.sh
@@ -1,11 +1,22 @@
-echo "Converting all pptx to pdf (this may take some time)"
-unoconv -f pdf *.pptx
-unoconv -f pdf *.ppt
+# Get the directory of this script so it can be run from anywhere
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
+shopt -s nullglob # In case there are no pptx or ppt files
+
+echo "Converting all pptx to pdf (this may take some time)"
+for f in *.{pptx,ppt}; do
+    BASE_FILENAME=${f%%.*} # Just get the name of the file, no extension
+    if [ ! -f "$BASE_FILENAME.pdf" ]; then
+        echo " + Converting $f..."
+        unoconv -f pdf $f
+    fi
+done
+
+shopt -u nullglob # Undo that option
 echo "Adding title pages to all pdfs"
 for f in *.pdf
 do
-        python title_gen.py "$f" | pandoc --from latex -o "${f}__titlepage.pdf";
+        python $DIR/title_gen.py "$f" | pandoc --from latex -o "${f}__titlepage.pdf";
         stapler sel "${f}__titlepage.pdf" "$f" "${f}__withtitlepage.pdf";
 done
 
@@ -17,4 +28,3 @@ rm *__titlepage.pdf
 rm *__withtitlepage.pdf
 
 echo "Created pdf out.pdf"
-
